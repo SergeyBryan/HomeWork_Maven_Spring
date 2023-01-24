9bcf5f2ee4603737336aa484bf0e1a62e5e3a3de
sergeybryan <regkmastis@yandex.ru>
2023-01-24 14:13:16 +0300

    Initial commit

---
+++ src/main/java/com/mavenspringhomework/homework_maven_spring/HomeWorkMavenSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mavenspringhomework.homework_maven_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkMavenSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkMavenSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mavenspringhomework/homework_maven_spring/HomeWorkMavenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavenspringhomework.homework_maven_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkMavenSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
